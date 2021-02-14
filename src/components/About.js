53f20044cbbe4c36ff17223a9510d07831075b89
Brian Peiris <brianpeiris@gmail.com>
2021-02-14 04:54:27 -0500

    Add back button to about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,20 @@ import * as components from '../../bink/src/components/index.js';
 export default class About extends Component {
 	constructor() {
 		super();
+		this.addClass('about');
+
 		new components.HeadingComponent(undefined, {
 			dom: dom.h2(),
 			text: 'About',
 		}).appendTo(this);
+
 		new components.TextComponent(undefined, {
 			text: 'SomaField lets you build and collect music visualizations.',
 		}).appendTo(this);
-		this.addClass('about');
+
+		new components.ButtonComponent(undefined, {
+			text: 'Back',
+			anchor: '#',
+		}).appendTo(this);
 	}
 }
